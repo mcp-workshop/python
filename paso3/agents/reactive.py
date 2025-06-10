690ee697b254fd66b08f116525183f7f46a9ab37
sanguino <sanguino@me.com>
2025-06-11 00:03:18 +0200

    chore: igualar agentes

--- paso3/agents/reactive.py
+++ paso3/agents/reactive.py
@@ -4,7 +4,10 @@ from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
+from datetime import datetime
 import os
+import asyncio
+
 
 model = ChatOllama(model="qwen2.5")
 
@@ -17,29 +20,29 @@ async def main():
   
   async with stdio_client(server_params) as (read, write):
     async with ClientSession(read, write) as session:
-        await session.initialize()
-        tools = await load_mcp_tools(session)
-        agent = create_react_agent(model, tools)
-        
-        now = datetime.now()
-        fecha_actual = now.strftime("%-d de %B de %Y")
-        hora_actual = now.strftime("%H:%M")
-        
-        messages = [
-          {"role": "system", "content": "Eres un agente que responde preguntas sobre el tiempo."},
-          {"role": "system", "content": f"Hoy es {fecha_actual}, son las {hora_actual}."},
-          {"role": "system", "content": "Estas en Las Rozas de Madrid"},
-          {"role": "user", "content": "¿Qué temperatura va a hacer mañana?"}
-        ]
+      await session.initialize()
+      tools = await load_mcp_tools(session)
+      agent = create_react_agent(model, tools)
+      
+      now = datetime.now()
+      fecha_actual = now.strftime("%-d de %B de %Y")
+      hora_actual = now.strftime("%H:%M")
+      
+      messages = [
+        {"role": "system", "content": "Eres un agente que responde preguntas sobre el tiempo."},
+        {"role": "system", "content": "Se lo mas escueto posible, respondiendo unicamente con la pregunta del usuario. NO AÑADAS MAS INFORMACIÓN DE LA SOLICITADA, AUNQUE LA TENGAS."},
+        {"role": "system", "content": f"Hoy es {fecha_actual}, son las {hora_actual}."},
+        {"role": "system", "content": "Estas en Las Rozas de Madrid"},
+        {"role": "user", "content": "¿Qué temperatura va a hacer mañana?"}
+      ]
 
-        agent_response = None
-        async for agent_response in agent.astream({"messages": messages}):
-          print(agent_response)
-          print("-----------------")
+      agent_response = None
+      async for agent_response in agent.astream({"messages": messages}):
+        print(agent_response)
+        print("-----------------")
 
-        print("\nRESPUESTA FINAL:\n")
-        print(getattr(agent_response["agent"]["messages"][-1], "content", None))
+      print("\nRESPUESTA FINAL:\n")
+      print(getattr(agent_response["agent"]["messages"][-1], "content", None))
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
